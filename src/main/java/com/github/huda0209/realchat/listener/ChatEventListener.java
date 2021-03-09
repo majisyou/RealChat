1881f54afa279375b4188b2f83a047ca63f59409
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-03-09 16:57:08 +0900

    [add]プレイヤーの位置を取得するコードを追加

--- src/main/java/com/github/huda0209/realchat/listener/ChatEventListener.java
+++ src/main/java/com/github/huda0209/realchat/listener/ChatEventListener.java
@@ -1,11 +1,18 @@
 package com.github.huda0209.realchat.listener;
 
 import com.github.huda0209.realchat.RealChat;
+import com.github.huda0209.realchat.util.playerDistance;
 
 import com.github.ucchyocean.lc3.LunaChatAPI;
+import com.github.ucchyocean.lc3.japanize.JapanizeType;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+import java.util.List;
 
 
 public class ChatEventListener implements Listener {
@@ -19,7 +26,27 @@ public class ChatEventListener implements Listener {
 
     @EventHandler
     public void ChatEvent(AsyncPlayerChatEvent event){
+        Player player = event.getPlayer();
+        String message = event.getMessage();
 
+        Location playerLocation = player.getLocation();
+
+        World playerWorld = player.getWorld();
+        List<Player> worldPlayers = playerWorld.getPlayers();
+
+        String chatMessage =  lunaChatAPI.japanize(message, JapanizeType.GOOGLE_IME);
+
+        event.setCancelled(false);
+
+        for(Player worldPlayer : worldPlayers){
+            Location worldPlayerLocation = worldPlayer.getLocation();
+            if(playerDistance.isRangeIn(playerLocation,worldPlayerLocation,10)){
+                player.sendMessage(chatMessage);
+            }
+        }
+    }
+
+    public void SendMessage(){
 
 
     }
