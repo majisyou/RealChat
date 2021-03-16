216e26ec4e76e0c8317ae988b85f9a2375a52fdb
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-03-17 00:14:15 +0900

    [change]コンフィグをリロードするコマンドを削除し新たなクラスに変更 コンフィグをリロードするコマンドをクラスに移行し、このファイルの該当コードを削除

--- src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
+++ src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
@@ -2,11 +2,11 @@ package com.github.huda0209.realchat.command;
 
 import com.github.huda0209.realchat.RealChat;
 import com.github.huda0209.realchat.config.loadConfig;
+import com.github.huda0209.realchat.command.reloadConfig;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.Locale;
 
@@ -19,9 +19,6 @@ public class CommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-
-        Player player = (Player)sender;
-
         switch (args[0].toLowerCase(Locale.ROOT)){
             case "mode" :
 
@@ -30,44 +27,23 @@ public class CommandHandler implements CommandExecutor {
             //reload RealChat config file
             case "reload":
                 if(!sender.hasPermission("RealChat.Reload")){
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
-                    return true;
+                    sender.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
+                    break;
                 }
-                try {
-                    loadConfig.LoadConfigFile(plugin);
-
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§a Reload the config file.");
-                    System.out.println("Reload the config file.");
-
-                }catch(Exception e){
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§c While reload the config file, occurred error. Please check console.");
-                    System.out.println("While reload the config file, occurred error.");
-                    System.out.println(e.toString());
-                }
+                reloadConfig.Reload(sender,plugin);
                 break;
 
             //reload config file, RealChat and LunaChat
             case "reloadall":
                 if(!(sender.hasPermission("RealChat.Reload") && sender.hasPermission("lunachat-admin.reload"))){
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
-                    return true;
+                    sender.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
+                    break;
                 }
-                try {
-                    loadConfig.LoadConfigFile(plugin);
-                    plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "lc reload");
-
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§a Reload RealChat and LunaChat plugin's config file.");
-                    System.out.println("Reload RealChat and LunaChat plugin's config file.");
-
-                }catch(Exception e){
-                    player.sendMessage("§9[" + plugin.getDescription().getName() + "]§c While reload the config file, occurred error. Please check console.");
-                    System.out.println("While reload the config file, occurred error.");
-                    System.out.println(e.toString());
-                }
+                reloadConfig.ReloadAll(sender,plugin);
                 break;
 
             default:
-                player.sendMessage("§9[" + plugin.getDescription().getName() + "]§c Unknown command.");
+                sender.sendMessage("§9[" + plugin.getDescription().getName() + "]§c Unknown command.");
                 break;
         }
         return true;
