dbaa9a99aa09914815821c59ae809cf3d2dc88df
huda0209 <65341393+huda0209@users.noreply.github.com>
2021-03-18 23:26:36 +0900

    [fix]引数がない場合に、usageがでるように修正

--- src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
+++ src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
@@ -19,6 +19,9 @@ public class CommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if(args.length == 0) return false;
+
         switch (args[0].toLowerCase(Locale.ROOT)){
             case "mode" :
 
