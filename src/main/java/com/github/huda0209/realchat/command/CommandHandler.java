44a8a20361c277351cae156951a77a8129639587
huda0209 <huda030209@gmail.com>
2021-04-04 23:55:13 +0900

    [add]AdminModeコマンドとReserveNLTellコマンドを追加

--- src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
+++ src/main/java/com/github/huda0209/realchat/command/CommandHandler.java
@@ -1,12 +1,11 @@
 package com.github.huda0209.realchat.command;
 
 import com.github.huda0209.realchat.RealChat;
-import com.github.huda0209.realchat.config.loadConfig;
-import com.github.huda0209.realchat.command.reloadConfig;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Locale;
 
@@ -23,8 +22,23 @@ public class CommandHandler implements CommandExecutor {
         if(args.length == 0) return false;
 
         switch (args[0].toLowerCase(Locale.ROOT)){
-            case "mode" :
+            //AdminMode
+            case "adminmode" :
+                if(!sender.hasPermission("RealChat.AdminMode")){
+                    sender.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
+                    return true;
+                }
+                ModeChange.AdminModeChanger((Player) sender);
+                break;
 
+            //ReserveNLTell(ReserveNoLimitTell)
+            //change ReserveNoLimitTell mode
+            case "reservenltell" :
+                if(!sender.hasPermission("RealChat.ReserveNoLimitTellMode")){
+                    sender.sendMessage("§9[" + plugin.getDescription().getName() + "]§c You don't have permission!");
+                    return true;
+                }
+                ModeChange.ReserveNoLimitTellModeChanger((Player) sender);
                 break;
 
             //reload RealChat config file
@@ -36,6 +50,7 @@ public class CommandHandler implements CommandExecutor {
                 reloadConfig.Reload(sender,plugin);
                 break;
 
+            //reloadALL
             //reload config file, RealChat and LunaChat
             case "reloadall":
                 if(!(sender.hasPermission("RealChat.Reload") && sender.hasPermission("lunachat-admin.reload"))){
