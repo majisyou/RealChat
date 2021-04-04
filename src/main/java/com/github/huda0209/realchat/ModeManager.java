945275bb716b70df3cce568be67265d5167c2f9c
huda0209 <huda030209@gmail.com>
2021-04-04 23:56:28 +0900

    [add]ReserveNoLimitTellModeの管理コードを追加 getReserveNoLimitTellModeとetReserveNoLimitTellModeを追加

--- src/main/java/com/github/huda0209/realchat/ModeManager.java
+++ src/main/java/com/github/huda0209/realchat/ModeManager.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 
 public class ModeManager{
     private static HashMap<Player, Boolean> playerAdminMode = new HashMap<>();
+    private static HashMap<Player, Boolean> ReserveNoLimitTellMode = new HashMap<>();
 
+    //Admin mode
     public static boolean getAdminMode(Player player){
         if(!playerAdminMode.containsKey(player)){
             return false;
@@ -21,4 +23,22 @@ public class ModeManager{
             playerAdminMode.remove(player);
         }
     }
+
+
+    //Reserve No Limit Tell Mode
+    public static boolean getReserveNoLimitTellMode(Player player){
+        if(!ReserveNoLimitTellMode.containsKey(player)){
+            return true;
+        }
+        return ReserveNoLimitTellMode.get(player);
+    }
+
+    public static void setReserveNoLimitTellMode(Player player,Boolean bool){
+        if(!bool){
+            ReserveNoLimitTellMode.put(player,false);
+        }else{
+            ReserveNoLimitTellMode.remove(player);
+        }
+    }
+    //true: tellの受信を受け付ける  false: tellの受信を拒否する
 }
